2800340d1a7257811ce3e9be577869a16ced8a4e
Cameron Dziesinski <cdziesinski@gmail.com>
2020-02-25 00:59:09 -0500

    initial push

---
+++ CoffeeShop/Controllers/CoffeeShopController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    public class CoffeeShopController : Controller
+    {
+        public IActionResult Registration()
+        {
+            return View(new RegisterUser());
+        }
+        public IActionResult Summary(RegisterUser user)
+        {
+            //return View(user);
+            if (ModelState.IsValid)
+            {
+                return View(user);
+            }
+            else
+            {
+                return View("Registration", user);
+            }
+        }
+    }
+}
---
+++ CoffeeShop/Models/RegisterUser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Must be between 2 and 30 characters")]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(12, MinimumLength = 8, ErrorMessage = "Must be between 8 and 12 characters")]
+        public string Password { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$")]
+        public string Email { get; set; }
+        [Required]
+        public DateTime DOB { get; set; }
+        [Required]
+        public bool Human { get; set; }
+
+        
+    }
+}
